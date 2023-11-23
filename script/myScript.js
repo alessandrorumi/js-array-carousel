33e395682470b2e77e6fd0393799ba8240139f95
alessandrorumi <rumi.alessandro21@gmail.com>
2023-11-23 16:51:51 +0100

    Feature: Arrows responsive in base all'img

--- script/myScript.js
+++ script/myScript.js
@@ -14,19 +14,45 @@ const arrowUp = document.querySelector('.previous');
 arrowDown.addEventListener('click', function() {
 
   if (activeItem < items.length - 1) {
-
+    // Rimozione Classe 'active'
     items[activeItem].classList.remove('active');
 
     activeItem++;
-
+    // Aggiunta Classe 'active'
     items[activeItem].classList.add('active');
 
   }
-
+    // Se è alla fine dell'array
     if (activeItem === items.length - 1) {
-
+      // Aggiunta Classe 'hidden'
       arrowDown.classList.add('hidden');
-
     }
+
+    // Al click di arrowDown se arrowUp è !== 0
+    arrowUp.classList.remove('hidden');
+
+});
+
+// ArrowUp hidden
+arrowUp.classList.add('hidden');
+
+// Al click di Arrow Up
+arrowUp.addEventListener('click', function() {
+  // Rimozione Classe 'active'
+  items[activeItem].classList.remove('active');
+
+  activeItem--;
+
+  if (activeItem >= 0) {
+    // Aggiunta Classe 'active'
+    items[activeItem].classList.add('active');
+  }
+
+  if (activeItem === 0) {
+    arrowUp.classList.add('hidden');
+  }
+
+  // Al click di arrowUp se activeItem !== items.length - 1
+  arrowDown.classList.remove('hidden');
 
 });
